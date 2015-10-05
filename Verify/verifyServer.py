becee533110c247328151f57584ba73d88c81e6a
lixu19950414 <980858415@qq.com>
2015-10-05 13:26:01 +0800

    correct verifyServer using new myTCPServer

--- Verify/verifyServer.py
+++ Verify/verifyServer.py
@@ -2,10 +2,9 @@
 
 # coding=uft-8
 
-from lib.myTCPServer import MyTCPServer
+from lib.myTCPServer import ThreadedTCPServer, ThreadedTCPRequestHandler
 import socket
 import threading
-from SocketServer import BaseServer, BaseRequestHandler, ThreadingMixIn
 import Defines.network as network
 
 
@@ -13,7 +12,7 @@ g_sIPV4Addr = network.g_sIPV4
 g_nIPV4Port = network.g_nIPV4Port
 g_sIPV6Addr = network.g_sIPV6
 g_nIPV6Port = network.g_nIPV6Port
-g_dConnected = {}  # This dictionary contains ((af, socktype, proto, canonname, sa), hostname) as elements.
+g_dConnected = {}  # This dictionary contains ((af, socktype, proto, canonname, sa): hostname) as elements.
 g_pLock = threading.Lock()
 
 
@@ -25,7 +24,7 @@ def SendMessage(res, message):
     pSocket.close()
 
 
-class ThreadedTCPRequestHandler(BaseRequestHandler):
+class VerifyServerRequestHandler(ThreadedTCPRequestHandler):
     nMessageID = 0
     """
     Server's request handler.
@@ -36,23 +35,16 @@ class ThreadedTCPRequestHandler(BaseRequestHandler):
         hostname, res, loginorout = eval(data)
         global g_pLock, g_dConnected
         if g_pLock.acquire():
-            ThreadedTCPRequestHandler.nMessageID += 1
+            VerifyServerRequestHandler.nMessageID += 1
             if loginorout is True:
                 g_dConnected[res] = hostname  # In case same hostname.
             else:
                 del g_dConnected[res]
-            sSend = str(ThreadedTCPRequestHandler.nMessageID) + str(g_dConnected)
+            sSend = str(VerifyServerRequestHandler.nMessageID) + str(g_dConnected)
         g_pLock.release()
         for addr in g_dConnected.keys():
             t = threading.Thread(target=SendMessage, args=(addr, sSend))
             t.start()
-
-
-class ThreadedTCPServer(ThreadingMixIn, MyTCPServer):
-    """
-    This is the mixture of MyTCPServer and ThreadingMixIn.
-    This class inherits MyTCPServer's __init__ method.
-    """
 
 
 def main():
@@ -61,7 +53,7 @@ def main():
         g_sIPV6Addr, g_nIPV6Port, socket.AF_INET6,
         socket.SOCK_STREAM, 0, socket.AI_PASSIVE)
     af, socktype, proto, canonname, sa = res[0]
-    server = ThreadedTCPServer(sa, ThreadedTCPRequestHandler)
+    server = ThreadedTCPServer(sa, VerifyServerRequestHandler)
     server_thread = threading.Thread(target=server.serve_forever)
     server_thread.daemon = True
     server_thread.start()
